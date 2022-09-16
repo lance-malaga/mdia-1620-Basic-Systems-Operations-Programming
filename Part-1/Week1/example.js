96608c7f9b87d2ed848ed60bd92a795339ad21b4
Lance Malaga <lmalaga@my.bcit.ca>
2022-09-16 12:15:24 -0700

    adding more code

--- Part-1/Week1/example.js
+++ Part-1/Week1/example.js
@@ -1,3 +1,32 @@
 "testing" 
 
 var car;
+
+// camel case
+var bugsCollected = 4;
+
+// pascal case
+var BugsCollected = 4;
+
+// snake case
+var Bugs_Collected = 4;
+
+var electronicsBox = "cpu";
+
+// Boolean
+var proceed = true;
+var i = 0;
+
+while(proceed) {
+    i++;
+    proceed = false;
+}
+console.log(i);
+
+// Data Type - Object
+var grannySmith = {
+    color: "green",
+    originLocation: "australia",
+    originDate: 1868,
+};
+console.log(grannySmith.color);
